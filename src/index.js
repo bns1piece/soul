ab54e5f85151b66baca26f19aed309b015e65c5f
Rain Byun <ragubyun@gmail.com>
2018-12-28 14:18:57 +0900

    setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
