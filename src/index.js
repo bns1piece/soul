9ac4374116549a05f76d5104fa2337f21c763234
devori <devori.lee@gmail.com>
2019-01-01 20:37:10 +0900

    add express monitoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import express from 'express';
 import path from 'path';
+import express from 'express';
+import expressStatusMonitor from 'express-status-monitor';
 
 import router from './routes';
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
+app.use(expressStatusMonitor());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.json());
 app.use('/api/v1', router);
